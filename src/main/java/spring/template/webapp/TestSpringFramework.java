38e1342cd3d02228d1c87dd9a9339868a65f9879
incompr <88598531+incompr@users.noreply.github.com>
2023-02-25 19:58:41 +0300

    initial

---
+++ src/main/java/spring/template/webapp/TestSpringFramework.java
@@ -0,0 +1,14 @@
+package spring.template.webapp;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringFramework {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+
+}
